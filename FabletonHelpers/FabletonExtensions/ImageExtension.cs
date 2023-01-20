a8aadc23a82144fae8c72c8c4a613ef39bbc58a8
Fabian Trejo Malpica <LoRd_FAB1@hotmail.com>
2023-01-20 14:11:54 -0600

    Added ImageExtension.ToByteArray

---
+++ FabletonHelpers/FabletonExtensions/ImageExtension.cs
@@ -0,0 +1,25 @@
+﻿using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.Advanced;
+using SixLabors.ImageSharp.Formats;
+
+namespace FabletonImageSharpExtensions
+{
+    public static class ImageExtension
+    {
+        public static byte[] ToByteArray(this Image image, IImageEncoder encoder)
+        {
+            try
+            {
+                using (var ms = new MemoryStream())
+                {
+                    image.Save(ms, encoder);
+                    return ms.ToArray();
+                }
+            }
+            catch (Exception ex)
+            {
+                return new byte[0];
+            }
+        }
+    }
+}
---
+++ FabletonHelpers/TestProject/ImageExtensionTests.cs
@@ -0,0 +1,27 @@
+﻿
+using FabletonImageSharpExtensions;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.Formats.Jpeg;
+using SixLabors.ImageSharp.Formats.Png;
+using SixLabors.ImageSharp.PixelFormats;
+
+namespace TestProject
+{
+    public class ImageExtensionTests
+    {
+        [Fact]
+        public void ToByteArray_EmptyTest()
+        {
+            byte[] bytes = ImageExtension.ToByteArray(null, null);
+            Assert.Equal(0, bytes.Length);
+        }
+
+        [Fact]
+        public void ToByteArray_MinTest()
+        {
+            Image<Rgba32> logo = Image.Load<Rgba32>("Resources/ImageTest1.jpg");
+            byte[] bytes = ImageExtension.ToByteArray(logo, new JpegEncoder() { });
+            Assert.Equal(256548, bytes.Length);
+        }
+    }
+}
